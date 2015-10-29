a2d80639bd0989fef1ceb9ab002ec1b336a2a307
Rich Wareham <rjw57@cam.ac.uk>
2015-10-29 22:00:43 +0000

    fix incorrect run-kicad.sh script

--- container/run-kicad.sh
+++ container/run-kicad.sh
@@ -5,5 +5,4 @@ useradd -m --uid "${USER_UID}" \
 	--gid "${USER_GID}" "${USER_NAME}"
 mkdir -p "/home/${USER_NAME}/.config/"
 ln -s "/kicad/config" "/home/${USER_NAME}/.config/kicad"
-su -c "bash" -l "${USER_NAME}"
-#su -c "kicad '${KICAD_PROJECT}'" -l "${USER_NAME}"
+su -c "kicad '${KICAD_PROJECT}'" -l "${USER_NAME}"
